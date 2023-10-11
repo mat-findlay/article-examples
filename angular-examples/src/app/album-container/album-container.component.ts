cb62d76f307e26f7ec80437fda9e2b2aafe2de7d
Mat Findlay <MFindlay@jetblue.com>
2023-10-11 15:10:36 -0400

    Added component reuse with ngFor

--- angular-examples/src/app/album-container/album-container.component.ts
+++ angular-examples/src/app/album-container/album-container.component.ts
@@ -7,11 +7,27 @@ import { Album } from '../album/album.types';
   styleUrls: ['./album-container.component.css']
 })
 export class AlbumContainerComponent {
-  album: Album = {
+  albumList: Album[] = [
+  {
     id: 1,
     title: 'No Sleep \'Til New Providence',
     artist: 'Ghost Pirate',
     cover: '../../assets/ghost-pirate-thumb.png',
     releaseDate: 'Jan 1, 2006',
-  }
+  },
+  {
+    id: 2,
+    title: 'Nifty As I Want To Be',
+    artist: 'Nifty Cal',
+    cover: '../../assets/mic-thumb.png',
+    releaseDate: 'Jan 1, 2006',
+  },
+  {
+    id: 1,
+    title: 'New Day(Yesterday)',
+    artist: 'Jethro Null',
+    cover: '../../assets/flute-thumb.jpeg',
+    releaseDate: 'Jan 1, 2006',
+  },
+]
 }
