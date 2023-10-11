b1370f81851c9cf09b868f942c3eee29960ad301
Mat Findlay <MFindlay@jetblue.com>
2023-10-11 11:14:34 -0400

    Added a base component

--- angular-examples/src/app/app.module.ts
+++ angular-examples/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule
