a10c1b2bc3f0b3869e5831945234a664a04e15d0
Mat Findlay <MFindlay@jetblue.com>
2023-10-13 09:56:05 -0400

    Added search

--- angular-examples/src/app/album-container/album-container.component.ts
+++ angular-examples/src/app/album-container/album-container.component.ts
@@ -7,34 +7,51 @@ import { Album } from '../album/album.types';
   styleUrls: ['./album-container.component.css']
 })
 export class AlbumContainerComponent {
+  filteredAlbumList: Album[] = [];
   albumList: Album[] = [
-  {
-    id: 1,
-    title: 'No Sleep \'Til Nassau',
-    artist: 'Ghost Pirate',
-    cover: '../../assets/ghost-pirate-thumb.png',
-    releaseDate: 'Jan 1, 2006',
-  },
-  {
-    id: 2,
-    title: 'Nifty As I Want To Be',
-    artist: 'Nifty Cal',
-    cover: '../../assets/mic-thumb.png',
-    releaseDate: 'Jan 1, 2006',
-  },
-  {
-    id: 1,
-    title: 'day = new Day(Yesterday)',
-    artist: 'Jethro Null',
-    cover: '../../assets/flute-thumb.jpeg',
-    releaseDate: 'Jan 1, 2006',
-  },
-  {
-    id: 1,
-    title: 'I Wanna Be Your Bogg',
-    artist: 'Pete Bogg',
-    cover: '../../assets/flute-thumb.jpeg',
-    releaseDate: 'Jan 1, 2006',
-  },
-]
+    {
+      id: 1,
+      title: 'No Sleep \'Til Nassau',
+      artist: 'Ghost Pirate',
+      cover: '../../assets/ghost-pirate-thumb.png',
+      releaseDate: 'Jan 1, 2006',
+    },
+    {
+      id: 2,
+      title: 'Nifty As I Want To Be',
+      artist: 'Nifty Cal',
+      cover: '../../assets/mic-thumb.png',
+      releaseDate: 'Jan 1, 2006',
+    },
+    {
+      id: 1,
+      title: 'day = new Day(Yesterday)',
+      artist: 'Jethro Null',
+      cover: '../../assets/flute-thumb.jpeg',
+      releaseDate: 'Jan 1, 2006',
+    },
+    {
+      id: 1,
+      title: 'I Wanna Be Your Bogg',
+      artist: 'Pete Bogg',
+      cover: '../../assets/flute-thumb.jpeg',
+      releaseDate: 'Jan 1, 2006',
+    },
+  ];
+
+  constructor() {
+    this.filteredAlbumList = this.albumList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredAlbumList = this.albumList;
+    }
+
+    this.filteredAlbumList = this.albumList.filter(
+      album => 
+        album?.title.toLowerCase().includes(text.toLowerCase()) || 
+        album?.artist.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
